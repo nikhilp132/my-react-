169c0a372a8d926ee8b09dd1f0ee4c63ec99f08a
nikhilp132 <nikhilparadkar200@gmail.com>
2022-05-25 09:37:21 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>My Dashboard</h1>, document.getElementById('root'));
+var d = new Date(),
+  year = d.getFullYear(),
+  month = d.getMonth() + 1,
+  day = d.getDate(),
+  hours = d.getHours(),
+  minutes = d.getMinutes(),
+  seconds = d.getSeconds(),
+  a = hours > 12 ? 'PM' : 'AM',
+  hours = hours > 12 ? hours - 12 : hours;
+
+ReactDOM.render(
+  <>
+    <h1>My Dashboard</h1>
+    <p>todays date is: {`${day}/${month}/${year}`}</p>
+    <p>Current Time is: {`${hours}:${minutes}:${seconds} ${a}`}</p>
+  </>,
+  document.getElementById('root')
+);
