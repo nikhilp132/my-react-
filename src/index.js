b1443499a4291d1d213fd31bf355aa1214fd67c8
nikhilp132 <nikhilparadkar200@gmail.com>
2022-05-27 12:18:53 +0530

    2 files modified

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
+var heading = {
+  color: 'blue',
+  fontSize: '70px',
+};
+
 ReactDOM.render(
   <>
-    <h1 className="heading">My Dashboard</h1>
+    <h1 className="heading" style={heading}>
+      My Dashboard
+    </h1>
+    <div className="listContainer">
+      <p className="listCreater">+ Create a List</p>
+    </div>
   </>,
   document.getElementById('root')
 );
